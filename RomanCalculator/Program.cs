2090208472c7bc81c5ad51c577087becc240bfa8
Pavel <sculltakers111@gmail.com>
2023-02-27 18:52:43 +0300

    first commit

--- RomanCalculator/Program.cs
+++ RomanCalculator/Program.cs
@@ -2,18 +2,9 @@
 using RomanLibrary;
 
 var calculate = new Calculate();
+var result = calculate.Evaluate("(IV + CM) / II");
+Console.WriteLine(result);
 
-//var result = calculate.Evaluate("(IV + CM) / II");
-// var result = calculate.ArabicIntoRoman(2048);
-// Console.WriteLine(result);
-
-
-// int a = 10;
-// int b = 11;
-//
-// int c = a / b;
-//
-// Console.WriteLine(c);
 
 
 
